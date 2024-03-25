ec692762fa0733913cbdb71139080b9735aeb13c
bft-dojo-dev <164850844+bft-dojo-dev@users.noreply.github.com>
2024-03-25 18:48:14 +0900

    Update index_error.py

--- index_error.py
+++ index_error.py
@@ -1,13 +1,8 @@
 """this is a test for pylint"""
-fruits_list = ["orange", "apple", "banana"]
-print(fruits_list[3]) #index_error
 
-test = fruits_list[3]
-print(test)
-
+"""return a+b"""
 def add(a, b):
     return a + c  # 'c' は未定義
   
-def multiply(a, b):
-    return a * b
-print(multiply("Hello", 3))  # 文字列と整数の乗算はできない
+addition = add(5,10)
+print(addition)
