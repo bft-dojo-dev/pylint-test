7af5808854911be8e2ad748d5f0eb1d1812070fd
bft-dojo-dev <164850844+bft-dojo-dev@users.noreply.github.com>
2024-03-25 18:44:25 +0900

    Update index_error.py

--- index_error.py
+++ index_error.py
@@ -4,3 +4,10 @@ print(fruits_list[3]) #index_error
 
 test = fruits_list[3]
 print(test)
+
+def add(a, b):
+    return a + c  # 'c' は未定義
+  
+def multiply(a, b):
+    return a * b
+print(multiply("Hello", 3))  # 文字列と整数の乗算はできない
